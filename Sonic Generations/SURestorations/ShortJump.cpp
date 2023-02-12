c14e8399b1a251d09bda519fed7933f461665a45
loco <61364136+locogit@users.noreply.github.com>
2023-02-12 15:46:39 -0500

    fixed short jump

--- Sonic Generations/SURestorations/ShortJump.cpp
+++ Sonic Generations/SURestorations/ShortJump.cpp
@@ -1,15 +1,15 @@
-bool shortJumpMove = false;
 int lastHurdleIndex = 0;
 int currentHurdleIndex = 1;
 
-HOOK(int, __fastcall, ShortJumpMove, 0x11BF200, int This) {
+bool ShortJumpMove() {
 	Sonic::Player::CPlayerSpeedContext* sonic = Sonic::Player::CPlayerSpeedContext::GetInstance();
 	float velXZ = sonic->m_Velocity.x() + sonic->m_Velocity.z();
-	shortJumpMove = abs(velXZ) >= 5;
-	if (shortJumpMove) {
-		WRITE_JUMP(0x11BF008, (void*)0x11BF03A);
-		WRITE_JUMP(0x11BF247, (void*)0x11BF275);
+	return  abs(velXZ) >= 5;
+}
 
+void playAnimBegin() {
+	Sonic::Player::CPlayerSpeedContext* sonic = Sonic::Player::CPlayerSpeedContext::GetInstance();
+	if (ShortJumpMove()) {
 		currentHurdleIndex = (lastHurdleIndex == 0) ? 1 : 0;
 		lastHurdleIndex = currentHurdleIndex;
 
@@ -17,12 +17,50 @@ HOOK(int, __fastcall, ShortJumpMove, 0x11BF200, int This) {
 		sonic->ChangeAnimation(hurdleAnimName);
 	}
 	else {
-		// reverse the jumps
+		sonic->ChangeAnimation("JumpShortBegin");
 	}
+}
 
-	return originalShortJumpMove(This);
+void playAnimTop() {
+	Sonic::Player::CPlayerSpeedContext* sonic = Sonic::Player::CPlayerSpeedContext::GetInstance();
+	if (!ShortJumpMove()) {
+		sonic->ChangeAnimation("JumpShortTop");
+	}
+}
+
+void __declspec(naked) ShortTop()
+{
+	static uint32_t shortTop = 0x011BF036;
+	__asm
+	{
+		call playAnimTop
+		jmp[shortTop]
+	}
+}
+
+void __declspec(naked) ShortBegin()
+{
+	static uint32_t shortTop = 0x011BF26E;
+	__asm
+	{
+		call playAnimBegin
+		jmp[shortTop]	
+	}
+}
+
+HOOK(void, __fastcall, SonicShortJumpUpdate, 0xE6BF20, Sonic::Player::CPlayerSpeed* This, void* _, const hh::fnd::SUpdateInfo& updateInfo) {
+	originalSonicShortJumpUpdate(This, _, updateInfo);
+	if (BlueBlurCommon::IsModern()) {
+		Sonic::Player::CPlayerSpeedContext* sonic = This->GetContext();
+		Hedgehog::Base::CSharedString state = This->m_StateMachine.GetCurrentState()->GetStateName();
+		Hedgehog::Base::CSharedString anim = sonic->GetCurrentAnimationName();
+		if (state == "JumpHurdle")
+			lastHurdleIndex = (anim == "JumpHurdleL") ? 1 : 0;
+	}
 }
 
 void ShortJump::Install() {
-	INSTALL_HOOK(ShortJumpMove);
+	INSTALL_HOOK(SonicShortJumpUpdate);
+	WRITE_JUMP(0x011BF008, ShortTop);
+	WRITE_JUMP(0x011BF247, ShortBegin);
 }
